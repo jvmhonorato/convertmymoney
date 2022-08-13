c95d04926bd768ad55d623e5452115b58d5ed36c
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-08-12 23:21:50 -0300

    setting index.ejs
    instructions in tutorial.txt and in the file itself

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
+//imports
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const path = require('path')
 
-//configuração responsável por direcionar consulta a pasta view ao tipo de arquivo ejs
+//Static files
+app.use(express.static("public"));
+app.use(express.static(__dirname +"public/css"));
+app.use(express.static(__dirname +"public/js"));
+app.use(express.static(__dirname +"public/img"));
+
+
+//configuração responsável por direcionar consulta a pasta view ao tipo de arquivo ejs TEMPLATE ENGINE 
+app.set(expressLayouts)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 
-//Rota "home"
-app.get('/', (req, res) => { 
+
+
+//Navigation
+    app.get('/', (req, res) => { 
        res.render('home')
     })
+    app.get('/about', (req, res) => { 
+        res.render('about')
+     })
 
 
 
 
 
-
-
+//listen PORT
 app.listen(3000, err=>{
     if(err){
         console.log('could not start application')
